9c89adaa4a53e6be09e293ccf31d6d187e6d0771
Nikita Khavkin <nikitakhavkin@gmail.com>
2024-11-05 17:04:13 +0100

    Add Hello.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import Hello from "./components/Hello.tsx";
-import Projekt from "./components/Projekt.tsx";
-import Car from "./components/Car.tsx";
-import Prikol from "./components/Prikol.tsx";
-import Bild from "./components/Bild.tsx";
+
 
 function App() {
 
@@ -11,11 +7,6 @@ function App() {
       <h1 className="text-5xl text-green-500">
           CampusGo
       </h1>
-        <Hello />
-        <Projekt />
-        <Car model={"BMW"}/>
-        <Prikol />
-        <Bild image={"https://img.goodfon.com/wallpaper/nbig/9/9e/krasivyy-krasivaya-koshka-kot.webp"}/>
     </>
   )
 }
--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,17 +1,19 @@
+// rsc
 
+interface HelloProps {
+    message: string
+}
 
-const Hello = () => {
+const Hello = ({message}: HelloProps) => {
     const number = 2
-
-    const newNumber = number + 3
 
     return (
         <div className="border-2 border-blue-500">
             <h1 className="text-2xl text-blue-600">
-                Hello World
+                {message}
             </h1>
             <p>
-                {number} + 3 = {newNumber}
+                {number}
             </p>
         </div>
     );
