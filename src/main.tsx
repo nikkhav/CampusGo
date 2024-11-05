cc4c10c8f7471a535f8d8d5abdef9ac902030c69
Nikita Khavkin <nikitakhavkin@gmail.com>
2024-11-06 00:24:06 +0100

    Add react router and GitHub actions

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
---
+++ src/pages/Home.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>CampusGo</div>;
+};
+
+export default Home;
