3ca45cef9f93c8a87a53b8d2513a2b2566dd4f29
Nikita Khavkin <nikitakhavkin@gmail.com>
2024-11-06 01:25:54 +0100

    Install fonts and add Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,16 @@
 const Home = () => {
-  return <div>CampusGo</div>;
+  return (
+    <div>
+      <h2 className="text-6xl text-center mt-32 wide-font">
+        <span className="half-highlight">Nachhaltig</span> fahren und <br />
+        Zeit sparen
+      </h2>
+      <p className="text-2xl text-center font-inter font-light mt-12">
+        Reduziere deine Fahrtkosten und lerne neue Freunde aus deiner Nähe
+        kennen – alles auf dem Weg zur Uni.
+      </p>
+    </div>
+  );
 };
 
 export default Home;
