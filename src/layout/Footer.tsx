2696cc2feb4a64291abee8e00f71c4bb25003091
EduRus <ed72048551@gmail.com>
2024-11-14 13:32:05 +0100

    Add footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,6 +1,17 @@
+import uni_logo from "../assets/uni-bayreuth-logo.png";
+import fim_logo from "../assets/fim-logo.svg";
+import fhg_logo from "../assets/fhg-logo.svg";
+
 const Footer = () => {
-  // TODO: Add footer here ...
-  return <div></div>;
+    return (
+        <div className="flex flex-col justify-center items-center bg-gray-100 w-full mt-10">
+            <div className="flex justify-between items-center w-7/12 mt-10 mb-5">
+                <img className="w-1/4 h-auto" src={uni_logo} alt={"Uni Bayreuth"}/>
+                <img className="w-1/4 h-auto" src={fim_logo} alt={"FIM"}/>
+                <img className="w-1/4 h-auto" src={fhg_logo} alt={"FHG"}/>
+            </div>
+        </div>
+    );
 };
 
 export default Footer;
