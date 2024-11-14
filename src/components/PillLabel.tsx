c74e7a708394c2e735e2784ca0db70a7f79e4ecd
EduRus <ed72048551@gmail.com>
2024-11-14 14:55:12 +0100

    Finish Home page

--- src/components/PillLabel.tsx
+++ src/components/PillLabel.tsx
@@ -1,7 +1,7 @@
 export default function PillLabel({ text }: { text: string }) {
     return (
         <div className="inline-flex items-center justify-center rounded-full border border-gray-200 bg-white px-4 py-1.5 text-sm font-medium text-gray-900">
-            <p>{text}</p>
+            <p className="text-lg">{text}</p>
         </div>
     )
 }
